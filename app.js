b4d15cf0bd3e3e068b1890aa46d7729fded70c76
Kutay <81999664+drainoff35@users.noreply.github.com>
2023-05-21 16:18:19 +0300

    Static files uploaded.

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-const fn = () => {
-  console.log('Function working...');
+const express = require('express');
+const app = express();
+const path = require('path');
+const myLogger = (req, res, next) => {
+  console.log('Middleware Log 1');
+  next();
 };
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware Log 2');
+  next();
+};
+
+app.use(express.static('public'));
+app.use(myLogger);
+app.use(myLogger2);
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı...`);
+});
